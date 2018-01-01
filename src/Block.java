39b527bb566cbc02f2f89b720f83574d2433ca9b
Sean McDonnell <sean@freshpow.io>
2017-12-31 22:22:42 -0500

    updated block java

--- src/Block.java
+++ src/Block.java
@@ -1,11 +1,34 @@
-import java.lang 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
+/**
+ * Created by ivanliljeqvist on 2017-08-31.
+ */
 public class Block {
-  private int previousHash;
-  private String[] transactions
-}
-public class main {
 
-}
+    private int previousHash;
+    private String[] transactions;
 
-public static void main[String]
+    private int blockHash;
+
+    public Block(int previousHash, String[] transactions) {
+        this.previousHash = previousHash;
+        this.transactions = transactions;
+
+        Object[] contens = {Arrays.hashCode(transactions), previousHash};
+        this.blockHash = Arrays.hashCode(contens);
+
+    }
+
+    public int getPreviousHash() {
+        return previousHash;
+    }
+
+    public String[] getTransaction() {
+        return transactions;
+    }
+
+    public int getBlockHash() {
+        return blockHash;
+    }
+}
